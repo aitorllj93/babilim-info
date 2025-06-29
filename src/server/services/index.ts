3019b221c07d02861b1bb713bb5c99e71e42ae43
aitorllamas <aitorllj93@gmail.com>
2025-06-30 00:52:21 +0200

    fix server path

--- src/server/services/index.ts
+++ src/server/services/index.ts
@@ -5,8 +5,8 @@ import { join } from "node:path";
 import { KBRepository } from "./KBRepository";
 import { ThumbnailRepository } from "./ThumbnailRepository";
 
-const kbRepository = new KBRepository(join(homedir(), "Projects", "babilim", "src", "content", "vault"));
-const thumbnailRepository = new ThumbnailRepository(join(homedir(), "Projects", "babilim", "src", "content", "vault"));
+const kbRepository = new KBRepository(join(homedir(), "Projects", "babilim-info", "src", "content", "vault"));
+const thumbnailRepository = new ThumbnailRepository(join(homedir(), "Projects", "babilim-info", "src", "content", "vault"));
 
 export {
   kbRepository,
